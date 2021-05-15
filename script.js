ab11c55f11df1d3f1efa9c63da25c01fce05e015
Josh <jdogcrane@gmail.com>
2021-05-15 16:59:31 -0400

    set up some more intelligent logic with an obj var for password

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ length: 8,
 numeric: false,
 special: false,
 uppercase: false,
+lowercase: false,
 };
 
 // all letters the generator will use
@@ -16,25 +17,46 @@ uniques = ["`","~","!", "@","#","$","%","^","&","*","(",")","-","_","=","+","[",
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 // Password Generator Function
-function createPassword() {
-
+window.onload = function createPassword() {
+// if user does not input anything sets length to 8
 // ask for character range
-password.length = prompt("Please insert the password length you desire between 8-128. If you do not wish to choose 8 will be the default number");
+password.length = prompt("Please insert the password length you desire between 8-128. If you do not wish to choose 8 will be the default number") || alert("length set to 8");
+if (!password.length) {
+ password.length = 8;
 
-// if user does not specify sets length to 8
-if (!verify) {
- alert("you need a value. I have not figured out how to default yet");
+console.log(password.length)
 
- // if the number does not fit the allowed criteria then prompt will be repeated
+
 } else if (password.length < 8 || password.length > 128) {
 password.length = prompt("Please insert the password length you desire between 8-128. If you do not wish to choose 8 will be the default number");
 
-
 } else {
-password.numeric = confirm("Want numbers in the password?");
-password.special = confirm("Want special characters in the password?");
-password.uppercase = confirm("Want uppercase in the password?");
+ // if user wants numeric then password.numeric is switched to true
+ password.numeric = confirm("Do you Want numbers in the password?");
+ // if user wants special then password.special is switched to true
+ password.special = confirm("Do you Want special characters in the password?");
+ // if user wants special then password.uppercase is switched to true
+ password.uppercase = confirm("Do you Want uppercase in the password?");
+ // if user wants special then password.lowercase is switched to true
+ password.lowercase = confirm("Do you want lowercase in the password?");
+ 
+ console.log(password.numeric)
+ console.log(password.special)
+ console.log(password.uppercase)
+ console.log(password.lowercase)
+ // Any other actions would leave it as false. since false && false = False
 };
+
+
+
+
+
+
+
+
+
+
+
 
 
 // if user wants password with numeric and special and uppercase then do = grab list of numbers and special characters and uppercase. 
