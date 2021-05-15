b5b0b8f4aa205c8a778c241bf7f77c837264a451
Josh <jdogcrane@gmail.com>
2021-05-15 14:31:28 -0400

    added var's and first function for createPassword!

---
+++ script.js
@@ -0,0 +1,37 @@
+
+var ask;
+// password criteria obj
+var password = {
+length: 8,
+numeric: true,
+special: true
+};
+
+// all possible characters
+characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+uniques = ["`","~","!", "@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","]","}",",","<",".",">","?","/",];
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+function createPassword() {
+// ask for character range
+ask = prompt("Please insert the password length you desire between 8-128. If you do not wish to choose 8 will be the default number");
+// if user does not specify sets length to 8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
