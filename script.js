657ddf7f7ef7438e04f7286f704e3f881ff2ffbb
Josh <jdogcrane@gmail.com>
2021-05-16 15:27:38 -0400

    Added more comments to explain things better

--- script.js
+++ script.js
@@ -7,48 +7,40 @@ var password = {
  lowercase: false,
  generatedPassword: [],
 };
+
 // Puts everything that has been set to true based off confirms into one array
 var container = [];
-
 // all letters the generator will use
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+// all capital letters the generator will use
 capLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 // all special characters the generator will use
-
 uniques = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", " ? ", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
-
 // all numbers the generator will use
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
  var passwordFinal = createPassword();
  var passwordText = document.querySelector("#password");
-
  passwordText.value = passwordFinal;
 }
-
-
- // Add event listener to generate button
- generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Password Generator Function
 function createPassword() {
+
  // if user does not input anything sets length to 8
  // rounds number if number is decimal
- // ask for character range
  password.length = Math.round(prompt("Please insert the password length you desire between 8-128. If you do not wish to choose 8 will be set as the length.")) || 8;
 
- console.log(password.length)
-// prevents password from being too small or big
+ // prevents password from being too small or big
  if (password.length < 8 || password.length > 128) {
   password.length = alert("That number was too big or too small. Please Choose a number between 8-128 next time. Thanks!");
-
+  //allows user to select the criteria they want
  } else {
   // if user wants numeric then password.numeric is switched to true
   password.numeric = confirm("Do you want numbers in the password?");
@@ -58,12 +50,6 @@ function createPassword() {
   password.uppercase = confirm("Do you want uppercase in the password?");
   // if user wants special then password.lowercase is switched to true
   password.lowercase = confirm("Do you want lowercase in the password?");
-
-  console.log(password.numeric);
-  console.log(password.special);
-  console.log(password.uppercase);
-  console.log(password.lowercase);
-  // Any other actions would leave it as false. since false && false = False
  };
 
  // if true throw it in the container
@@ -75,7 +61,6 @@ function createPassword() {
   password.length--;
  }
 
- console.log(container);
  // if true throw it in the container
  if (password.special) {
   container = container.concat(uniques);
@@ -85,7 +70,6 @@ function createPassword() {
   password.length--;
  }
 
- console.log(container);
  // if true throw it in the container
  if (password.uppercase) {
   container = container.concat(capLetters);
@@ -94,9 +78,8 @@ function createPassword() {
   // removes 1 from length if statement is true to make room for guaranteed
   password.length--;
  }
- console.log(container);
 
-
+// if true throw it in the container
  if (password.lowercase) {
   container = container.concat(letters);
   // Grabs 1 random guaranteed letter if statement is true
@@ -105,9 +88,9 @@ function createPassword() {
   password.length--;
  }
 
+ // if user declines all options this will notify them that nothing will be generated
  if (!password.lowercase && !password.uppercase && !password.numeric && !password.special) {
   alert("You selected no on all options. No password will be generated. Have a nice day!")
-
  }
 
  // grabs at random from the container into the generated password array
@@ -115,7 +98,9 @@ function createPassword() {
   var randomIndex = Math.floor(Math.random() * container.length);
   password.generatedPassword.push(container[randomIndex])
  }
-// clears containers for next generator
+
+ // clears containers for next generation if one is requested without a page request
  container = [];
+ // returns to the function the array of generated password and puts it in a string
  return password.generatedPassword.join("");
 }
